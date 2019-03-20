9542b822da7dda12bf81a8d9628023f7884ccefa
thevinh <binkool281132442@gmail.com>
2019-03-20 09:17:25 +0700

    them moi file mvc

---
+++ lab4th/lab4th/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab4th.MvcApplication" Language="C#" %>
---
+++ lab4th/lab4th/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab4th.Startup))]
+namespace lab4th
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
